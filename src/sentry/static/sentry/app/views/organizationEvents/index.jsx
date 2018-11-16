a26f02728de6ce4310b97dfd91e0580fecea168c
Billy Vong <billy@sentry.io>
2018-11-16 10:23:03 -0800

    feat(org-events): Add UTC to URL params
    Refactors `useUtc` prop name to `utc` to be consistent with URL (and to
    compress URL params a bit)

--- src/sentry/static/sentry/app/views/organizationEvents/index.jsx
+++ src/sentry/static/sentry/app/views/organizationEvents/index.jsx
@@ -4,11 +4,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'react-emotion';
 
-import {DEFAULT_STATS_PERIOD} from 'app/constants';
+import {DEFAULT_STATS_PERIOD, DEFAULT_USE_UTC} from 'app/constants';
 import {defined} from 'app/utils';
 import {getLocalDateObject, getUtcDateString} from 'app/utils/dates';
-import {getParams} from 'app/views/organizationEvents/utils';
-import EventsContext from 'app/views/organizationEvents/eventsContext';
 import Feature from 'app/components/acl/feature';
 import HeaderSeparator from 'app/components/organizations/headerSeparator';
 import MultipleEnvironmentSelector from 'app/components/organizations/multipleEnvironmentSelector';
@@ -18,13 +16,16 @@ import TimeRangeSelector from 'app/components/organizations/timeRangeSelector';
 import space from 'app/styles/space';
 import withOrganization from 'app/utils/withOrganization';
 
+import {getParams} from './utils/getParams';
+import EventsContext from './utils/eventsContext';
+
 class OrganizationEventsContainer extends React.Component {
   static propTypes = {
     organization: SentryTypes.Organization,
     router: PropTypes.object,
   };
 
-  static getInitialStateFromRouter(props) {
+  static getStateFromRouter(props) {
     const {query} = props.router.location;
     const hasAbsolute = !!query.start && !!query.end;
     let project = [];
@@ -48,17 +49,13 @@ class OrganizationEventsContainer extends React.Component {
       end = getLocalDateObject(end);
     }
 
-    const values = {
+    return {
       project,
       environment,
       period: query.statsPeriod || (hasAbsolute ? null : DEFAULT_STATS_PERIOD),
       start: start || null,
       end: end || null,
-    };
-
-    return {
-      ...values,
-      queryValues: {...values},
+      utc: typeof query.utc !== 'undefined' ? query.utc === 'true' : DEFAULT_USE_UTC,
     };
   }
 
@@ -69,7 +66,25 @@ class OrganizationEventsContainer extends React.Component {
       updateParams: this.updateParams,
     };
 
-    this.state = OrganizationEventsContainer.getInitialStateFromRouter(props);
+    const values = OrganizationEventsContainer.getStateFromRouter(props);
+    this.state = {
+      ...values,
+      queryValues: {
+        ...values,
+      },
+    };
+  }
+
+  componentWillReceiveProps(nextProps, nextState) {
+    if (this.props.location !== nextProps.location) {
+      const values = OrganizationEventsContainer.getStateFromRouter(nextProps);
+
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState({
+        ...values,
+        queryValues: {...values},
+      });
+    }
   }
 
   updateParams = obj => {
@@ -110,14 +125,15 @@ class OrganizationEventsContainer extends React.Component {
     }));
   };
 
-  handleChangeTime = ({start, end, relative}) => {
-    this.setState({start, end, period: relative});
+  handleChangeTime = ({start, end, relative, utc}) => {
+    this.setState({start, end, period: relative, utc});
   };
 
   handleUpdatePeriod = () => {
-    this.setState(({period, start, end, ...state}) => {
+    this.setState(({period, start, end, utc, ...state}) => {
       let newValueObj = {
         ...(defined(period) ? {period} : {start, end}),
+        utc,
       };
 
       this.updateParams(newValueObj);
@@ -157,7 +173,7 @@ class OrganizationEventsContainer extends React.Component {
 
   render() {
     const {organization, children} = this.props;
-    const {period, start, end} = this.state;
+    const {period, start, end, utc} = this.state;
 
     const projects =
       organization.projects && organization.projects.filter(({isMember}) => isMember);
@@ -190,6 +206,7 @@ class OrganizationEventsContainer extends React.Component {
                 relative={period}
                 start={start}
                 end={end}
+                utc={utc}
                 onChange={this.handleChangeTime}
                 onUpdate={this.handleUpdatePeriod}
               />
